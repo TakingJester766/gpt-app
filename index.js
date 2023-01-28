abe46ae3a207eca9c68fd2082e0e0fd13aa90cd5
Alex Zimmerman <ZimmermanAlex13@gmail.com>
2023-01-27 22:45:53 -0500

    Ability to ask specific questions about text.

---
+++ index.js
@@ -0,0 +1,135 @@
+// A express server, which will handle api requests come in and respond with a json object, it will use body parser as well as cors to handle the requests.
+
+
+// You can now call the getSlices function in other files like this:
+const { getSlices } = require('./src/WebScrape.js');
+const config = require('./config.js');
+const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const AsyncTest = require('./src/AsyncTest.js');
+const WebScrape = require('./src/WebScrape.js');
+const app = express();
+const port = 3001;
+
+const configuration = new Configuration({
+    organization: config.organization,
+    apiKey: config.apiKey
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let isSummary = true;
+
+isSummary ? app.post('/', async (req, res) => {
+
+    let responseSummary = "";
+
+    try {
+        const textArr = await WebScrape.getSlices();
+        try {
+            for (let i = 0; i < textArr.length; i++) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            console.log(textArr[i]);
+            const { message } = req.body;
+            const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `You will be given a block of text. Following this block of text, the user will prompt you with a question. Answer it as best you can. 
+                
+                Text block: ${textArr[i]}
+
+                If you are unable to answer the question or are unsure, please respond with "I don't know".
+            
+                User question: ${message}`,
+            max_tokens: 200,
+            temperature: 0,
+            });
+            console.log(response.data);
+            if (response.data.choices[0].text) {
+            
+                responseSummary = responseSummary + ' ' + JSON.stringify(response.data.choices[0]);
+                console.log(responseSummary);
+            }
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+
+    try {
+        const textArr = await WebScrape.getSlices();
+        try {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `You will be given a block of text. Following this block of text summarize the text. 
+                
+                Text block: ${responseSummary}
+
+                If you are unable to answer the question or are unsure, please respond with "I don't know"`,
+            max_tokens: 500,
+            temperature: 0,
+            });
+            console.log(response.data);
+            if (response.data.choices[0].text) {
+                res.json({
+                message: response.data.choices[0].text
+                });
+            }
+            
+        } catch(err) {
+            console.log(err);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+    
+    //console.log(slices[0]);
+}) : app.post('/', async (req, res) => {
+
+    let responseSummary = "";
+
+    try {
+        const textArr = await WebScrape.getSlices();
+        try {
+            for (let i = 0; i < textArr.length; i++) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            console.log(textArr[i]);
+            const { message } = req.body;
+            const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `You will be given a block of text. Following this block of text, the user will prompt you with a question. Answer it as best you can. 
+                
+                Text block: ${textArr[i]}
+
+                If you are unable to answer the question or are unsure, please respond with "I don't know".
+            
+                User question: ${message}`,
+            max_tokens: 200,
+            temperature: 0,
+            });
+            console.log(response.data);
+            if (response.data.choices[0].text) {
+            
+                responseSummary = responseSummary + ' ' + JSON.stringify(response.data.choices[0]);
+                console.log(responseSummary);
+            }
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ src/WebScrape.js
@@ -0,0 +1,59 @@
+const puppeteer = require('puppeteer');
+
+//getSlices();
+module.exports = {
+  getSlices: async () => {
+    console.log("Inside of WebScrape.js...")
+    let slices = [];
+
+  // Launch a headless Chrome instance
+  const browser = await puppeteer.launch();
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  // Navigate to the website
+  await page.goto("https://cis.org/Report/ForeignBorn-Population-Hits-Nearly-48-Million-September-2022");
+
+  // Wait for the page to load
+  await page.waitForSelector("p");
+
+  // Get the text of all the <p> tags on the page
+  const p_tags = await page.evaluate(() => {
+    const tags = document.querySelectorAll("p");
+    return Array.from(tags).map(tag => tag.textContent);
+  });
+
+  // Print the text of each <p> tag
+  let textArr = [];
+
+
+
+    for (let i = 0; i < p_tags.length; i++) {
+        let j = 0;
+
+        while (p_tags[i].length > 0) {
+            if (textArr[j] == null) {
+                textArr[j] = "";
+            }
+        
+            if (textArr[j].length + p_tags[i].length >= 18000) {
+                let remainingLength = 10000 - textArr[j].length;
+                textArr[j] += p_tags[i].substring(0, remainingLength);
+                p_tags[i] = p_tags[i].substring(remainingLength);
+                j++;
+            } else {
+                textArr[j] += p_tags[i];
+                p_tags[i] = "";
+            }
+        }
+    }
+
+  //console.log(slices[0]);
+  
+  // Close the browser
+  await browser.close();
+  
+  return textArr;
+  }
+} 
